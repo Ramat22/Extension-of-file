a3597f59665cf708dd41a82f0b2f0013bf1d7d84
Ramat22 <87207342+Ramat22@users.noreply.github.com>
2021-07-10 21:36:52 +0530

    Extension of file
    From filename, extension is separated from file

---
+++ extension.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 10 21:26:31 2021
+
+@author: win10
+"""
+
+filename=input("Input filename:")
+f_ext=filename.split(".")
+print("extension of file is:"+repr(f_ext[-1]))
